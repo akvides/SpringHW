64dffdc683228de554f0167e073b22c71c5f3aed
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-05-13 15:58:03 +0300

    Initial Commit

---
+++ SpringHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringHW
+//
+//  Created by Василий Полторак on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
