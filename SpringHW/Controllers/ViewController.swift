9403f16b5873972497a617ec0ccea5ce4feca3d0
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-05-13 16:23:00 +0300

    Настройка сториборда Подключение аутлетов

--- SpringHW/Controllers/ViewController.swift
+++ SpringHW/Controllers/ViewController.swift
@@ -5,15 +5,21 @@
 //  Created by Василий Полторак on 13.05.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animateView: SpringView!
+    @IBOutlet var animationValuesLabel: [UILabel]!
+    @IBOutlet weak var startAnimationButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startAnimation() {
+    }
+    
 }
 
